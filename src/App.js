7e61f4be01c40209c61be98dad0f8a4bb01d60b1
Hammad Mirza <132938772+hammadmirzaa@users.noreply.github.com>
2024-08-02 07:49:07 +0000

    added boiler plate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Pagination from './Pagination';
+import Search from './Search';
+import Stories from './Stories';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      Welcome to Tech  News
+    </div>
+    <Search/>
+    <Pagination/>
+    <Stories/>
+    </>
+  );
+}
+
+export default App;
