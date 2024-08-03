d26f15a4521de740460be282edb4ad59a2227ff6
hammadmirzaa <hamadbaig25@gmail.com>
2024-08-03 13:30:35 +0000

    added css

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react'
-import { useGlobalContext } from './Context'
+import React, { useEffect } from "react";
+import { useGlobalContext } from "./Context";
 
 const Stories = () => {
   const context = useGlobalContext();
-  const {hits, nbPages} = context
+  const { hits, nbPages } = context;
   return (
-    <div> <h2>My Tech Posts</h2>
-      {
-        hits.map((hit) => {
-          return <h2>{hit.title}</h2>
-        })
-      }
+    <div>
+      <h2>My Tech Posts</h2>
+      <div className="stories-div">
+      {hits.map((hit) => {
+        const {title, author, objectID, url, num_comments} = hit;
+        return (
+          <>
+          <div className="card" key={objectID} >
+            <h2>{title}</h2>
+            <p>
+              By <span> {author} </span>  | <span>{num_comments}</span> comments
+            </p>
+            <div className="card-button">
+              <a href={url} target="_blank" >Read More</a>
+              <a href="#">Remove</a>
+            </div>
+          </div>
+          </>
+        );
+      })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stories
+export default Stories;
