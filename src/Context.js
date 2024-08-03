a1b9b096b47c15eb2a7b4e5fbcf8fdc62944263b
hammadmirzaa <hamadbaig25@gmail.com>
2024-08-03 09:12:54 +0000

    Added useReducer and trasnfered the fetch function in context file

--- src/Context.js
+++ src/Context.js
@@ -1,8 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
+import reducer from './reducer'
+
+let API = "https://hn.algolia.com/api/v1/search?"
+
+
+
+const initialState = {
+    query: "HTML",
+    nbPages: 0,
+    page: 0,
+    hits: [],
+}
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
+
+const AppProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+
+    const fetchApiData = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            // console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchApiData(`${API}query=${state.query}&page${state.page}`)
+    }, []);
+
     return (
         <AppContext.Provider value={"This is Hammad here"} >
             {children}
@@ -14,4 +45,4 @@ const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
-export {AppContext, AppProvider, useGlobalContext}
+export { AppContext, AppProvider, useGlobalContext }
