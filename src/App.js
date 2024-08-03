b94b11370f8bc7c75722f2da22c4cd71dd9239c9
hammadmirzaa <hamadbaig25@gmail.com>
2024-08-03 09:33:08 +0000

    integrated Api using useReducer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import Pagination from './Pagination';
 import Search from './Search';
 import Stories from './Stories';
-import { useGlobalContext } from './Context';
 
 function App() {
-  const data = useGlobalContext()
-  
+
   return (
     <>
     <div className="App">
-      Welcome to Tech  News {data}
+      Welcome to Tech  News 
     </div>
     <Search/>
     <Pagination/>
--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react'
+import { useGlobalContext } from './Context'
 
 const Stories = () => {
-
-
+  const context = useGlobalContext();
+  const {hits, nbPages} = context
   return (
-    <div>Stories</div>
+    <div> <h2>My Tech Posts</h2>
+      {
+        hits.map((hit) => {
+          return <h2>{hit.title}</h2>
+        })
+      }
+    </div>
   )
 }
 
